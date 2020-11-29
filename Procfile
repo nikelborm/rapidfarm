7b8241f52850a113e6bc7100dbc97732532c00f5
nikelborm <r0075301net.r@yandex.ru>
2020-11-29 20:21:45 +0300

    Bugfixes and others improvements (I dont remember)

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node startprod.js
+web: node server.js
--- server.js
+++ server.js
@@ -24,19 +24,30 @@ function createEmptyResponseData() {
     };
     return { resdata, rp: resdata.report };
 }
-
-
-
+function closeConnection( wsConnection, reason ) {
+    let { resdata, rp } = createEmptyResponseData();
+    rp.info = reason;
+    wsConnection.send(JSON.stringify(resdata));
+    wsConnection.terminate();
+}
+function setMainFarm( name ) {
+    
+}
 const port = process.env.PORT || 3000;
 const mongoLink = process.env.MONGODB_URI || "mongodb://myUserAdmin:0000@localhost:27017/admin";
 const redisLink = process.env.REDIS_URL || "redis://admin:foobared@127.0.0.1:6379";
-const secretKey = process.env.SECRET || "wHaTeVeR123";
-const farmSecret = process.env.FARM_SECRET || "ec5d48de1fea693990a7f5eebd52c632c744d473d595d0eb883e55b7dec14327";
+const sessionSecretKey = process.env.SESSION_SECRET || "wHaTeVeR123";
+const cookieSecretKey = process.env.COOKIE_SECRET || "wHaTeVeR123";
+const farmSecrets = JSON.parse(process.env.FARM_SECRETS) || {
+    "ec5d48de1fea693990a7f5eebd52c632c744d473d595d0eb883e55b7dec14327" : "Лондонская ферма",
+    "d8a928b2043db77e340b523547bf16cb4aa483f0645fe0a290ed1f20aab76257" /* asdasdasd */: "Команда 2"
+};
 
 
 let dbClient;
 let users = {};
-let sensors = {};
+let mainFarm;
+let sensorsLogs = {};
 
 const app = express();
 const redisClient = redis.createClient(redisLink);
@@ -58,13 +69,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(session({
     store,
-    secret: secretKey,
+    secret: sessionSecretKey,
     resave: false,
     rolling: true,
     unset: "destroy",
     saveUninitialized: false
 }));
-app.use(cookieParser(secretKey));
+app.use(cookieParser(cookieSecretKey));
 app.use(favicon(__dirname + "/build/favicon.ico"));
 
 function logout(request, response) {
@@ -74,25 +85,26 @@ function logout(request, response) {
         response.redirect("/");
     });
 }
+app.get("/logout", logout);
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", (request, response) => {
     response.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.get("/logout", logout);
 
 app.post("/loginAsFarm", function (request, response) {
-    const { secret } = request.body;
+    const { secret, name } = request.body;
     let { resdata, rp } = createEmptyResponseData();
 
     if ( secret?.length !== 64 ) {
         rp.info = "Incorrect key format";
-        return response.json(resdata);
-    }
-    if( sha256(secret) !== farmSecret ) {
+    } else if ( name?.length ) {
+        rp.info = "Farm unnamed";
+    } else if ( sha256( secret ) in farmSecrets === false ) {
         rp.info = "Farm not registered";
     } else {
         request.session.isFarm = true;
+        request.session.name = name;
         rp.isError = false;
         rp.info = "Success authorization";
     }
@@ -127,7 +139,7 @@ app.post("/loginAsUser", function (request, response) {
         rp.info = "Успешная авторизация";
     }).catch((err) => {
         console.log(err);
-        rp.info = err.message;
+        rp.info = "Ошибка сервера";
     }).finally(() => {
         response.json(resdata);
     });
@@ -169,15 +181,16 @@ app.post("/registerAsUser", function (request, response) {
         };
         if (result.email === email) {
             resdata.reply.errorField = "email";
-            throw new Error("Эта почта занята. Если вы владелец, попробуйте <a href='/restore' style='color: #FFFFFF;'>восстановить аккаунт</a>.");
+            rp.info = "Эта почта занята. Если вы владелец, попробуйте <a href='/restore' style='color: #FFFFFF;'>восстановить аккаунт</a>.";
         }
-        return users.insertOne(userProfile); // Возвращаем промис
-    }).then((result)=>{
-        request.session.authInfo = result.ops[0];
-        rp.isError = false;
-        rp.info = "Регистрация успешна";
+        return users.insertOne(userProfile)
+        .then((result) => {
+            request.session.authInfo = result.ops[0];
+            rp.isError = false;
+            rp.info = "Регистрация успешна";
+        }); // Возвращаем промис
     }).catch((err) => {
-        rp.info = err.message;
+        rp.info = "Ошибка сервера";
     }).finally(() => {
         response.json(resdata);
     });
@@ -190,19 +203,67 @@ WSServer.on("connection", (connection, request) => {
         connection.isAlive = true;
     });
     const cookies = cookie.parse(request.headers.cookie);
-    const sid = "" + cookieParser.signedCookie(cookies["connect.sid"], secretKey);
-    // TODO: Подумать над тем, что сообщение может начать обрабатываться до то того как редис вернёт запись для валидации
-    store.get(sid, (err, session) => {
-        if (err) console.log(err);
+    const sid = cookieParser.signedCookie(cookies["connect.sid"], sessionSecretKey);
+    console.log('sid: ', sid);
+    console.log('cookies["connect.sid"]: ', cookies["connect.sid"]);
+    if ( !sid ) return closeConnection(connection, "Вы не авторизованы!");
 
-        if (!session || !session.authInfo || err) {
-            let { resdata, rp } = createEmptyResponseData();
-            rp.info = "Вы не авторизованы!";
-            connection.send(JSON.stringify(resdata));
-            connection.terminate();
+    store.get(sid, (err, session) => {
+        if (!session || err) {
+            console.log( session, err );
+            return closeConnection(connection, "Вы не авторизованы!");
+        }
+        if ( session.isFarm ) {
+            // Описание того что ферма может отправить на сервер:
+                // {"messageType": "event", "process": "oxidation",     "state": "started" }
+                // {"messageType": "event", "process": "oxidation",     "state": "finished"}
+                // {"messageType": "event", "process": "watering",      "state": "started" }
+                // {"messageType": "event", "process": "watering",      "state": "finished"}
+                // {"messageType": "event", "process": "lighting",      "state": "started" }
+                // {"messageType": "event", "process": "lighting",      "state": "finished"}
+                // {"messageType": "event", "process": "groundHeating", "state": "started" }
+                // {"messageType": "event", "process": "groundHeating", "state": "finished"}
+                // {"messageType": "event", "process": "waterHeating",  "state": "started" }
+                // {"messageType": "event", "process": "waterHeating",  "state": "finished"}
+                // {"messageType": "event", "process": "airHeating",    "state": "started" }
+                // {"messageType": "event", "process": "airHeating",    "state": "finished"}
+                // {"messageType": "criticalEvent", "problemWith": "groundTemperature",    "valueNow": Число, "criticalValue": Число }
+                // {"messageType": "criticalEvent", "problemWith": "waterTemperature",     "valueNow": Число, "criticalValue": Число }
+                // {"messageType": "criticalEvent", "problemWith": "airTemperature",       "valueNow": Число, "criticalValue": Число }
+                // {"messageType": "criticalEvent", "problemWith": "groundHumidity",       "valueNow": Число, "criticalValue": Число }
+                // {"messageType": "criticalEvent", "problemWith": "airHumidity",          "valueNow": Число, "criticalValue": Число }
+                // {"messageType": "criticalEvent", "problemWith": "groundOxidationState", "valueNow": Число, "criticalValue": Число }
+                // {"messageType": "criticalEvent", "problemWith": "waterOxidationState",  "valueNow": Число, "criticalValue": Число }
+                // {"messageType": "criticalEvent", "problemWith": "groundSaltState",      "valueNow": Число, "criticalValue": Число }
+                // {"messageType": "criticalEvent", "problemWith": "waterSaltState",       "valueNow": Число, "criticalValue": Число }
+                // {"messageType": "sensorLogs", "sensor": "groundTemperature",    "value": Число }
+                // {"messageType": "sensorLogs", "sensor": "waterTemperature",     "value": Число }
+                // {"messageType": "sensorLogs", "sensor": "airTemperature",       "value": Число }
+                // {"messageType": "sensorLogs", "sensor": "groundHumidity",       "value": Число }
+                // {"messageType": "sensorLogs", "sensor": "airHumidity",          "value": Число }
+                // {"messageType": "sensorLogs", "sensor": "groundOxidationState", "value": Число }
+                // {"messageType": "sensorLogs", "sensor": "waterOxidationState",  "value": Число }
+                // {"messageType": "sensorLogs", "sensor": "groundSaltState",      "value": Число }
+                // {"messageType": "sensorLogs", "sensor": "waterSaltState",       "value": Число }
+            connection.isFarm = session.isFarm;
+            connection.name = session.name;
+            connection.on("message", (input) => {
+                // TODO: сделать функцию обработчик для добавления новой фермы
+                // TODO: сделать функцию обработчик для установки активной фермы
+                // Все команды, что прилетают идут главной ферме. А чтобы отдать другой - нужно сначала переключить
+                // const { authInfo } = connection;
+                // TODO: Проверять не слишком ли большие данные, чтобы долго их не обрабатывать
+                console.log('Пришло в ws: ', JSON.parse(input.toString()));
+                // if (rp.info) return connection.send(JSON.stringify(resdata));
+            });
         } else {
+            // обработчики для команд и запросов пользователя
+
             connection.authInfo = session.authInfo;
-            connection.on("message", async (input) => {
+            connection.on("message", (input) => {
+                // TODO: сделать функцию обработчик для добавления новой фермы
+                // TODO: сделать функцию обработчик для установки активной фермы
+                // Все команды, что прилетают идут главной ферме. А чтобы отдать другой - нужно сначала переключить
                 // const { authInfo } = connection;
                 // TODO: Проверять не слишком ли большие данные, чтобы долго их не обрабатывать
                 console.log('Пришло в ws: ', JSON.parse(input.toString()));
@@ -216,6 +277,10 @@ const cleaner = setInterval(() => {
     WSServer.clients.forEach((connection) => {
         // Если соединение мертво, завершить
         if (!connection.isAlive) {
+            if (connection.isFarm) {
+                // TODO: Если отключилась ферма - уведомить всех по почте или как-то ещё, а то пиздец. Это либо отключение интернета либо электричества блять либо пожар нахуй и ферма горит либо залило водой плату или короткое замыкание от этого. Что бы ни было причиной - это пиздец. А на ферме кстати нужно сделать чтобы автоподключение происходило
+                // TODO: Сделать разустановку главной фермы, если подключена единственная
+            }
             return connection.terminate();
         }
         // обьявить все соединения мертвыми, а тех кто откликнется на ping, сделать живыми
@@ -232,7 +297,7 @@ mongoClient.connect((err, client) => {
 
     dbClient = client;
     users = client.db().collection("users");
-    // sensors = client.db().collection("sensors");
+    sensorsLogs = client.db().collection("sensorsLogs");
     server.listen(port, function(){
         console.log("Сервер слушает");
     });
