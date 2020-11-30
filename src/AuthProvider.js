8010bfb2c36a08ca24b0cb4276b303cb07136b83
nikelborm <kolya007.klass@gmail.com>
2020-11-30 14:34:12 +0300

    Some improvements

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import getCookie from "./getCookie";
+import loader from "./loader";
 
 export const AuthContext = React.createContext({
     isAuthorized: false,
@@ -15,19 +16,44 @@ class AuthProvider extends Component {
     }
     login = async ( email, password ) => {
         // Запрос на авторизацию
+        console.log('email, password: ', email, password );
+        // Запрос на авторизацию
+        const body = {
+            email,
+            password,
+        };
+        const responseData = await loader(body, "/loginAsUser");
+        console.log('responseData: ', responseData);
+        if ( responseData.report.isError ) return;
+        // Если всё проходит успешно:
+        // TODO: либо сервер либо клиент ставит новые куки
+        this.setState({
+            isAuthorized: true,
+            formType: "login",
+            formSubmittingHandler: this.login,
+            fullName: responseData.reply.fullName
+        });
     }
     register = async ( email, password, confirmPassword, fullName ) => {
         console.log('email, password, confirmPassword, fullName: ', email, password, confirmPassword, fullName);
         // Запрос на авторизацию
-
+        const body = {
+            email,
+            password,
+            confirmPassword,
+            fullName
+        };
+        const responseData = await loader(body, "/registerAsUser");
+        console.log('responseData: ', responseData);
+        if ( responseData.report.isError ) return;
         // Если всё проходит успешно:
         // TODO: либо сервер либо клиент ставит новые куки
-        /* this.setState({
+        this.setState({
             isAuthorized: true,
             formType: "login",
             formSubmittingHandler: this.login,
             fullName
-        }); */
+        });
     }
     state = {
         isAuthorized: false,
---
+++ src/loader.js
@@ -0,0 +1,21 @@
+// import serverError from './serverError';
+
+async function loader(body, address) {
+    try {
+        const data = await (await fetch(document.location.origin + address, {
+            method: "post",
+            body: JSON.stringify(body),
+            headers: new Headers({
+                "Content-Type": "application/json"
+            })
+        })).json();
+        console.log(data);
+        // const { isError, info } = data.report;
+        // const { errorField } = data.reply;
+        return data;
+    } catch (error) {
+        console.log('error: ', error);
+        return { report: { isError:true, info: "Ошибка подключения" } };
+    }
+}
+export default loader;
