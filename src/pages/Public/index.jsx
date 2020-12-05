ae5fe545caf585da996476228ab54fdbed57856c
nikelborm <kolya007.klass@gmail.com>
2020-12-06 00:23:22 +0300

    В поисках Дори (идеального решения)

--- src/pages/Public/index.jsx
+++ src/pages/Public/index.jsx
@@ -4,19 +4,24 @@ import AuthContext from "../../tools/AuthContext";
 import Container from "react-bootstrap/Container";
 import Process from "./components/Process";
 
-export class PublicPage extends Component {
-    static contextType = AuthContext;
+export class PublicContent extends Component {
     render() {
-        if ( this.context.isAuthorized ) {
-            return <Redirect to="/admin"/>;
-        }
-        // return "Страница с графиками и инфой о состоянии теплицы";
         return <Container>
             <Process title="Полив" activity={true}/>
             <Process title="Аэрация" activity={true}/>
             <Process title="Свет" activity={false}/>
             <Process title="Подогрев почвы"/>
         </Container>;
+    }
+}
+class PublicPage extends Component {
+    static contextType = AuthContext;
+    render() {
+        if ( this.context.isAuthorized ) {
+            return <Redirect to="/admin"/>;
+        }
+        // return "Страница с графиками и инфой о состоянии теплицы";
+        return <PublicContent/>;
     }
 }
 
