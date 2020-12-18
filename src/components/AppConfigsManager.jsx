a23607534f529e95d20c6070f503a03ba2f97d94
nikelborm <kolya007.klass@gmail.com>
2020-12-18 11:56:01 +0300

    fixed processes presentation

--- src/components/AppConfigsManager.jsx
+++ src/components/AppConfigsManager.jsx
@@ -2,29 +2,22 @@ import React, { Component } from "react";
 import { addMessageListener } from "../tools/SocketManager";
 
 export const AppConfigsContext = React.createContext( {
-    processActivities: {}
+    processes: {},
+    sensors: {}
 });
 
 class AppConfigsProvider extends Component {
     state = {
-        processActivities: {},
+        processes: {},
+        sensors: {}
     }
     componentDidMount() {
-        // запрос на получение списка процессов с их активностям
-        // потом подписаться на обновления по процессам
         addMessageListener((data)=> {
             // eslint-disable-next-line default-case
             switch ( data.class ) {
-                case "activitySyncPackage":
-                    this.setState({
-                        processActivities: data.package
-                    });
+                case "configPackage":
+                    this.setState( () => data.package );
                     break;
-                case "event":
-                    this.setState(prevstate => {
-                        prevstate.processActivities[ data.process ] = data.isActive;
-                        return prevstate;
-                    });
             }
         });
     }
@@ -32,9 +25,7 @@ class AppConfigsProvider extends Component {
     render() {
         return (
             <AppConfigsContext.Provider
-                value={ {
-                    ...this.state,
-                } }
+                value={ this.state }
                 children={ this.props.children }
             />
         );
--- src/pages/Public/components/ProcessList.jsx
+++ src/pages/Public/components/ProcessList.jsx
@@ -1,15 +1,44 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Container from "react-bootstrap/Container";
 import { AppConfigsContext } from "../../../components/AppConfigsManager";
 import Process from "./Process";
+import { addMessageListener } from "../../../tools/SocketManager";
 
-class ProcessList extends Component {
+class ProcessList extends PureComponent {
+    state = {}
+    componentDidMount() {
+        addMessageListener((data)=> {
+            // eslint-disable-next-line default-case
+            switch ( data.class ) {
+                case "activitySyncPackage":
+                    this.setState( () => data.package );
+                    break;
+                case "event":
+                    this.setState( prevState => {
+                        prevState[ data.process ] = data.isActive;
+                        return prevState;
+                    } );
+            }
+        });
+    }
     static contextType = AppConfigsContext;
     render() {
         return (
             <Container>
                 { Object.entries( this.context.processes ).map(
-                    ([title, activity]) => <Process title={title} isActive={isActive} isAvailable={isAvailable}/>
+                    ( [ long, isActive ] ) => {
+                        console.log( 'this.context.processes: ', this.context.processes );
+                        const { isAvailable, title } = this.context.processes.find(
+                            process => process.long === long
+                        );
+                        return (
+                            <Process
+                                title={ title }
+                                isActive={ isActive }
+                                isAvailable={ isAvailable }
+                            />
+                        )
+                    }
                 ) }
             </Container>
         );
