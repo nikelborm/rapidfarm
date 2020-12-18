0aacb617f696735ea0c9526b9f2dd90566e0dbd1
nikelborm <kolya007.klass@gmail.com>
2020-12-18 12:10:48 +0300

    asd

--- src/components/AppConfigsManager.jsx
+++ src/components/AppConfigsManager.jsx
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import { addMessageListener } from "../tools/SocketManager";
 
 export const AppConfigsContext = React.createContext( {
-    processes: {},
-    sensors: {}
+    processes: [],
+    sensors: []
 });
 
 class AppConfigsProvider extends Component {
     state = {
-        processes: {},
-        sensors: {}
+        processes: [],
+        sensors: []
     }
     componentDidMount() {
-        addMessageListener((data)=> {
+        addMessageListener( data => {
             // eslint-disable-next-line default-case
             switch ( data.class ) {
                 case "configPackage":
