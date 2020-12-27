acd9d2c958584b55e671e64bfcec6dc0579767e3
nikelborm <kolya007.klass@gmail.com>
2020-12-27 23:18:51 +0300

    asd

--- src/components/AuthManager.jsx
+++ src/components/AuthManager.jsx
@@ -12,14 +12,14 @@ export const AuthContext = React.createContext( {
     login: async () => {}
 });
 class AuthProvider extends Component {
-    constructor(props) {
-        super(props);
+    constructor( props ) {
+        super( props );
         if ( this.isAuthSessionChanged() ) {
             this.state = {
                 isAuthorized: false,
                 fullName: "",
             }
-            this.updateAuthStatus("false", "");
+            this.updateAuthStatus( "false", "" );
         } else {
             this.state = {
                 isAuthorized: localStorage.getItem( "isAuthorized" ) === "true" || false,
@@ -34,11 +34,11 @@ class AuthProvider extends Component {
             fullName
         } );
     }
-    updateAuthStatus = (isAuthorized, fullName) => {
-        localStorage.setItem( "isAuthorized", ""+isAuthorized);
-        localStorage.setItem( "fullName", fullName);
+    updateAuthStatus = ( isAuthorized, fullName ) => {
+        localStorage.setItem( "isAuthorized", "" + isAuthorized );
+        localStorage.setItem( "fullName", fullName );
     }
-    isAuthSessionChanged = () => false && (localStorage.getItem( "connect.sid" ) !== getCookie( "connect.sid" ));
+    isAuthSessionChanged = () => false && ( localStorage.getItem( "connect.sid" ) !== getCookie( "connect.sid" ) );
     logout = async () => {
         // запрос на выход чтобы сервер стёр сессию
         this.sendToWS( { class : "logout" } );
@@ -48,11 +48,11 @@ class AuthProvider extends Component {
         } );
     }
     sendToWS = body => {
-        console.log("body: ", body);
+        console.log( "body: ", body );
         if( isSocketAvailable() ) {
-            WSConnection.send(JSON.stringify(body));
+            WSConnection.send( JSON.stringify( body ) );
         } else {
-            alert("Соединение потеряно");
+            alert( "Соединение потеряно" );
         }
     }
     login = ( email, password ) => {
@@ -80,7 +80,7 @@ class AuthProvider extends Component {
         addMessageListener( data => {
             if ( data.class !== "loginAsUser" && data.class !== "registerAsUser" ) return;
             if ( data.report.isError ) {
-                alert(data.info);
+                alert( data.info );
             } else {
                 this.updateAuthState( {
                     isAuthorized: true,
