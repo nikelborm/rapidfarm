741079acb3c63712deefd9891720bb6057796bc9
nikelborm <kolya007.klass@gmail.com>
2021-01-08 02:30:18 +0300

    layout fixes

--- src/pages/Admin/components/TimingRow.jsx
+++ src/pages/Admin/components/TimingRow.jsx
@@ -4,6 +4,7 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import styled from "styled-components";
 import OneTimeChanger from "./OneTimeChanger";
+import { Button } from "react-bootstrap";
 
 const StyledCol = styled( Col )`
     max-width: 400px !important;
@@ -11,11 +12,9 @@ const StyledCol = styled( Col )`
 class TimingsRow extends Component {
     render() {
         const [ from, to ] = this.props.oneTiming;
-        console.log('TimingsRow ');
-        console.log('to: ', to);
-        console.log('from: ', from);
         return (
             <Row xs={ 1 } sm={ 1 } md={ 2 } lg={ 2 } xl={ 2 } >
+                <Button>Удалить { " -> " }</Button>
                 <StyledCol>
                     <OneTimeChanger
                         isEditMode={ this.props.isEditMode }
