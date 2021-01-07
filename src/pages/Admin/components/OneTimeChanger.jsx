8b731dc013533f13677773062f285b7ec64901b6
nikelborm <kolya007.klass@gmail.com>
2021-01-07 20:00:14 +0300

    layout fixes

--- src/pages/Admin/components/OneTimeChanger.jsx
+++ src/pages/Admin/components/OneTimeChanger.jsx
@@ -1,26 +1,56 @@
 import React, { Component } from "react";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import InputGroup from "react-bootstrap/InputGroup";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import styled from "styled-components";
 
+
+const StyledText = styled( InputGroup.Text )`
+    padding: .25rem .5rem;
+    font-size: .875rem;
+    line-height: 1.5;
+    border-radius: .2rem;
+    width: 40px;
+    text-align: center;
+    display: initial;
+`;
+const StyledInput = styled( FormControl )`
+    width: 40px !important;
+    text-align: center;
+`;
 class OneTimeChanger extends Component {
+    letters = [ "Ч", "М", "С" ];
     render() {
-        const { role } = this.props;
+        const { role, time } = this.props;
         return (
-            <>
-                <InputGroup.Prepend>
-                    <InputGroup.Text>
+            <ButtonToolbar className="justify-content-between mb-3">
+                <InputGroup size="sm">
+                    <StyledText>
                         { role }
-                    </InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                    name="command"
-                    type="text"
-                    placeholder="Не пишите, если не уверены"
-                />
-                <InputGroup.Append>
+                    </StyledText>
+                </InputGroup>
+                { time.map( ( elem, index ) => (
+                    <InputGroup size="sm">
+                        <InputGroup.Prepend>
+                            <StyledText>
+                                { this.letters[ index ] }
+                            </StyledText>
+                        </InputGroup.Prepend>
+                        <StyledInput
+                            name="fromHours"
+                            type="text"
+                            value={ ~~elem }
+                        />
+                    </InputGroup>
+                ) ) }
+                <ButtonGroup size="sm">
                     <Button variant="primary" type="submit">
-                        Отправить
+                        +
                     </Button>
-                </InputGroup.Append>
-            </>
+                </ButtonGroup>
+            </ButtonToolbar>
         );
     }
 }
